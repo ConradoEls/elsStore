8dcb3978fb54cf24c83d248eee108345014c767d
Conrado Els <conradoels@gmail.com>
2024-01-15 17:46:54 -0300

    fetching products at fakeStore API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
+
 const Home = () => {
+
+  const [items, setItems] = useState(null);
+
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(data => setItems(data))
+  },[])
   return (
     <Layout>
-      Home
-      <Card />
+      <div className="grid gap-5 grid-cols-4 w-full max-w-screen-xl ">
+      {
+        items?.map((item)=>{
+          return <Card key={item.id} data={item} />
+        })
+      }
+      </div>
     </Layout>
   );
 };
